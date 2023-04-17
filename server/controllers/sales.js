21500266787e3ecb6a422d1042228c90bfc4f77a
tiaotiaook <shihuaok@gmail.com>
2023-04-17 19:06:35 +0800

    init commit

---
+++ server/controllers/sales.js
@@ -0,0 +1,23 @@
+// import OverallStat from "../models/OverallStat";
+
+
+// export const getSales=async(req,res)=>{
+// try{
+//     const OverallStats=await OverallStat.find();
+//     res.status(200).json(OverallStats[0]);
+// } catch(error){
+//     res.status(404).json({message:error.message})
+// }
+// };
+
+import OverallStat from "../models/OverallStat.js";
+
+export const getSales = async (req, res) => {
+  try {
+    const overallStats = await OverallStat.find();
+
+    res.status(200).json(overallStats[0]);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
